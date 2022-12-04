a86f0568882ba4a0f450822be584512e6946a932
zMMiche <110943571+zMMiche@users.noreply.github.com>
2022-12-04 09:24:52 +0100

    Finished Theme Switch Button Added indicator for theme switch

--- commons/script.js
+++ commons/script.js
@@ -1,25 +1,25 @@
-const body = document.querySelector('body')
+// const body = document.querySelector('body')
 
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-        localStorage.setItem('theme', themeName);
-        document.documentElement.className = themeName;
-    }
-    // function to toggle between light and dark theme
-    function toggleTheme() {
-       if (localStorage.getItem('theme') === 'theme-dark'){
-           setTheme('theme-light');
-                body.classList.remove('dark-html');
-       } else {
-           setTheme('theme-dark');
-           body.classList.add('dark-html');
-       }
-    }
-    // Immediately invoked function to set the theme on initial load
-    (function () {
-       if (localStorage.getItem('theme') === 'theme-dark') {
-           setTheme('theme-dark');
-       } else {
-           setTheme('theme-light');
-       }
-    })();
+// // function to set a given theme/color-scheme
+// function setTheme(themeName) {
+//         localStorage.setItem('theme', themeName);
+//         document.documentElement.className = themeName;
+//     }
+//     // function to toggle between light and dark theme
+//     function toggleTheme() {
+//        if (localStorage.getItem('theme') === 'theme-dark'){
+//            setTheme('theme-light');
+//                 body.classList.remove('dark-html');
+//        } else {
+//            setTheme('theme-dark');
+//            body.classList.add('dark-html');
+//        }
+//     }
+//     // Immediately invoked function to set the theme on initial load
+//     (function () {
+//        if (localStorage.getItem('theme') === 'theme-dark') {
+//            setTheme('theme-dark');
+//        } else {
+//            setTheme('theme-light');
+//        }
+//     })();
