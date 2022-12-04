939c0c3cb94a36536dc140f378fb853af395306f
zMMiche <110943571+zMMiche@users.noreply.github.com>
2022-12-04 08:57:09 +0100

    Added PostCss, Renewed Theme switch button

--- commons/script.js
+++ commons/script.js
@@ -1,9 +1,25 @@
-const switchbutton = document.querySelector('#switch');
 const body = document.querySelector('body')
 
-switchbutton.addEventListener('click', switchTheme());
-
-function switchTheme() {
-        body.classList.toggle('dark-html')
-        console.log('negro')
-};
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+        localStorage.setItem('theme', themeName);
+        document.documentElement.className = themeName;
+    }
+    // function to toggle between light and dark theme
+    function toggleTheme() {
+       if (localStorage.getItem('theme') === 'theme-dark'){
+           setTheme('theme-light');
+                body.classList.remove('dark-html');
+       } else {
+           setTheme('theme-dark');
+           body.classList.add('dark-html');
+       }
+    }
+    // Immediately invoked function to set the theme on initial load
+    (function () {
+       if (localStorage.getItem('theme') === 'theme-dark') {
+           setTheme('theme-dark');
+       } else {
+           setTheme('theme-light');
+       }
+    })();
